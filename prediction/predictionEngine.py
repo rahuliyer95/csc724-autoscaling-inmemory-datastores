daa355dba4a633f46c8c25827121a0fd23132ca3
Akshay <raulakshay@gmail.com>
2019-04-16 01:19:46 -0500

    Integrate ARIMA & RNN

--- prediction/predictionEngine.py
+++ prediction/predictionEngine.py
@@ -5,12 +5,15 @@ from threading import Timer
 
 def predict():
     arima_result = arima.arima_model()
+    print(arima_result)
     # RNN()
 
-    send_data_producer(arima_result)
+    # send_data_producer(arima_result)
 
 def send_data_producer(result):
     producer.producer(result)
 
-predict()
-# threading.Timer(60*15,predict).start()
+
+while True:
+    predict()
+    time.sleep(15*60)
